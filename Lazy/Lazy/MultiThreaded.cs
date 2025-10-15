0a24dfff42dd63e9bd63899e7e971ee46296bd3b
Alena-pa <alena.pakhnusheva@gmail.com>
2025-10-15 20:51:25 +0300

    Add MultiThreaded

---
+++ Lazy/Lazy/MultiThreaded.cs
@@ -0,0 +1,36 @@
+﻿// <copyright file="ILazy.cs" company="Pakhnusheva Alena">
+// Copyright (c) Pakhnusheva Alena. All rights reserved.
+// </copyright>
+
+using System;
+using ILazy;
+
+namespace LazyCalculation;
+
+public class MultiThreaded<T> : ILazy<T>
+{
+    private Func<T> _supplier;
+    private T value;
+    private bool isEvaluated;
+    private readonly Lock _lock = new();
+
+    public T Get()
+    {
+        if (isEvaluated)
+        {
+            return value;
+        }
+
+        lock (_lock)
+        {
+            if (!isEvaluated)
+            {
+                value = _supplier();
+                isEvaluated = true;
+                _supplier = null;
+            }
+        }
+
+        return value;
+    }
+}
---
+++ Lazy/Lazy/SingleThreaded.cs
@@ -0,0 +1,38 @@
+﻿// <copyright file="ILazy.cs" company="Pakhnusheva Alena">
+// Copyright (c) Pakhnusheva Alena. All rights reserved.
+// </copyright>
+
+using ILazy;
+using System;
+
+namespace LazyCalculation;
+
+public class SingleThreaded<T> : ILazy<T>
+{
+    private Func<T> _supplier;
+    private T value;
+    private bool isEvaluared;
+
+    public SingleThreaded(Func<T> supplier)
+    {
+        _supplier = supplier ?? throw new ArgumentNullException(nameof(supplier)); 
+    }
+
+    public T Get()
+    {
+        if (isEvaluared)
+        {
+            return value;
+        }
+
+        if (_supplier != null)
+        {
+            value = _supplier();
+            isEvaluared = true;
+        }
+
+        _supplier = null;
+
+        return value;
+    }
+}
